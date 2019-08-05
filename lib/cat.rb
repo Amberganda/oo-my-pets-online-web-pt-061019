0a20cca7692c649f43a1c65b0309a687c8a53539
Amber Taranto <ambzzzz@gmail.com>
2019-08-05 01:37:29 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,11 +1,12 @@
 class Cat
 
-    attr_reader :name
+    attr_reader :name, :mood
     attr_accessor :owner
 
     def initialize(name, owner)
         @name = name
         @owner = owner
+        @mood = "nervous"
     end
 
 
