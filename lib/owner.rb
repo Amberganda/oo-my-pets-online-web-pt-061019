0aa9053c906cce11363ed20f92d9795e6437143f
Amber Taranto <ambzzzz@gmail.com>
2019-08-05 01:25:41 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,13 +1,11 @@
 class Owner
 
     #getters
-    attr_reader :name
+    attr_reader :name, :species
+    
 
     def initialize(name)
         @name = name
-    end
-
-    def species(animal_name)
         @species = "human"
     end
 end
